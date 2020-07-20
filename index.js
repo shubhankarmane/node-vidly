c2c88202a62f71361db5f82f947043dcb5a96951
shubhankarmane <onlinestudysam@gmail.com>
2020-07-20 18:27:41 +0530

    Moved the genres out into a database and modified the API to fetch it from there.

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ const express = require('express');
 const app = express();
 const genres = require('./routes/genres');
 
+
+// Connecting to mongodb once for the lifetime of the application
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/vidly')
+    .then(() => console.log('Connected!'))
+    .catch(err => console.error('Connection Failed', err));
+
 app.use(express.json());
 app.use('/api/genres', genres);
 
--- routes/genres.js
+++ routes/genres.js
@@ -1,22 +1,38 @@
 const express = require('express');
 const router = express.Router();
-
+// Joi is for validation
 const Joi = require('joi');
 
-const genres = [
-    { id: 1, genre: 'Action' },
-    { id: 2, genre: 'Horror' },
-    { id: 3, genre: 'Sci-Fi' },
-    { id: 4, genre: 'Romance' },
-    { id: 5, genre: 'Comedy' },
-    { id: 6, genre: 'Crime' },
-    { id: 7, genre: 'Adventure' },
-    { id: 8, genre: 'Drama' },
-    { id: 9, genre: 'Thriller' },
-    { id: 10, genre: 'Animation' },
-];
+// Connect to mongodb
+const mongoose = require('mongoose');
 
-router.get('/', (req, res) => {
+// Defining schema
+const Genre = mongoose.model('Genre', 
+    new mongoose.Schema({
+        genre: {
+            type: String,
+            required: true,
+        }
+    }
+));
+
+// const genres = [
+//     { id: 1, genre: 'Action' },
+//     { id: 2, genre: 'Horror' },
+//     { id: 3, genre: 'Sci-Fi' },
+//     { id: 4, genre: 'Romance' },
+//     { id: 5, genre: 'Comedy' },
+//     { id: 6, genre: 'Crime' },
+//     { id: 7, genre: 'Adventure' },
+//     { id: 8, genre: 'Drama' },
+//     { id: 9, genre: 'Thriller' },
+//     { id: 10, genre: 'Animation' },
+// ];
+
+// for GET requests
+router.get('/', async (req, res) => {
+    // sort the genres by their name and send it to the requester
+    const genres = await Genre.find().sort('genre');
     if(genres) {
         return res.status(200).send(genres);
     }
@@ -25,8 +41,9 @@ router.get('/', (req, res) => {
     }
 });
 
-router.get('/:id', (req, res) => {
-    const genre = genres.find(g => g.id === parseInt(req.params.id));
+router.get('/:id', async (req, res) => {
+    const genre = await Genre.findById(req.params.id);
+
     if(!genre) {
         return res.status(400).send('Bad Request, Invalid ID');
     }
@@ -35,55 +52,60 @@ router.get('/:id', (req, res) => {
     }
 });
 
-router.post('/', (req, res) => {
+// For POST requests
+router.post('/', async (req, res) => {
+    // Validation for the request body
     const schema = Joi.object({
         genre: Joi.string().required(),
     });
-
     const validationResult = schema.validate(req.body);
+    // End of validation
+
     if(validationResult.error) {
         return res.status(400).send('Genre is invalid.');
     }
     else {
-        const addGenre = {
-            id: genres.length + 1,
+        // Saving the genre to the server and returning the genre to the requester
+        let addGenre = new Genre({
             genre: req.body.genre
-        }
-        genres.push(addGenre);
+        });
+        addGenre = await addGenre.save();
         return res.status(200).send(addGenre);
     }
 });
 
-router.delete('/:id', (req, res) => {
-    const genre = genres.find(g => g.id === parseInt(req.params.id));
+// For DELETE requests
+router.delete('/:id', async (req, res) => {
+    // We do not verify if the object exists on the db, we delete it first using the id
+    const genre = await Genre.findByIdAndRemove(req.params.id);
+
     if(!genre) {
         return res.status(400).send('No such genre exists.');
     }
-    else {
-        genres.splice(genres.indexOf(genre), 1);
-        return res.status(200).send('Deleted Successfully.');
-    }
+
+    return res.status(200).send(genre);
 });
 
-router.put('/:id', (req, res) => {
-    const genre = genres.find(g => g.id === parseInt(req.params.id));
-    if(genre) {
-        const schema = Joi.object({
-            genre: Joi.string().required(),
-        });
-        const validatationResult = schema.validate(req.body);
-        if(validatationResult.error) {
-            return res.status(400).send('Invalid data received.');
-        }
-        else {
-            const index = genres.indexOf(genre);
-            genres[index] = { id: parseInt(req.params.id), genre: req.body.genre };
-            return res.status(200).send(genres[index]);
-        }
+// For PUT requests
+router.put('/:id', async (req, res) => {
+    // Validating the genre from the request body
+    const schema = Joi.object({
+        genre: Joi.string().required(),
+    });
+    const validatationResult = schema.validate(req.body);
+    if(validatationResult.error) {
+        return res.status(400).send('Invalid data received.');
     }
-    else {
-        return res.status(404).send('No such genre.');
-    }
+    
+    // We do not verify if the object exists on the db, we update it first using the id
+    const genre = await Genre.findByIdAndUpdate(req.params.id, {
+        genre: req.body.genre
+    }, { new: true });
+    // The object with the 'new' property returns the updated object
+    
+    if(!genre) return res.status(404).send('Genre with the ID does not exist');
+
+    res.send(genre);
 });
 
 module.exports = router;
